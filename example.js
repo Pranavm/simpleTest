418e9091dff7ac345a34456c225e9347ef18d342
Pranavm <pranavm1932@gmail.com>
2014-10-08 12:18:54 +0530

    initial commit

---
+++ example.js
@@ -0,0 +1,33 @@
+var Nemo = require("../");
+
+/*
+process.env.nemoData = JSON.stringify({
+    targetBrowser: "firefox",
+    targetServer: "localhost",
+    serverProps: {"port": 4444},
+    seleniumJar: "/usr/bin/selenium-server-standalone.jar",
+    targetBaseUrl: "https://www.paypal.com"
+});
+*/
+
+var config = {
+    nemoData: {
+        targetBrowser: "firefox",
+        targetServer: "localhost",
+        serverProps: {
+            "port": 4444
+        },
+        seleniumJar: "/usr/bin/selenium-server-standalone.jar",
+        targetBaseUrl: "https://www.paypal.com"
+    }
+};
+//THE ABOVE OR BELOW WILL WORK TO SET nemoData. IN A CONTEST, SETTING VIA Nemo() WILL WIN
+
+(new Nemo(config)).setup().then(function(nemo) {
+    nemo.driver.get(nemo.props.targetBaseUrl);
+    nemo.driver.sleep(5000).
+    then(function() {
+        console.info("Nemo was successful!!");
+        nemo.driver.quit();
+    });
+});
